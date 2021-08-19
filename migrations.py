c41c40b415c08af3b3896a01cba12f38d0dc22fb
Llane Rost <llanerost@gmail.com>
2021-08-18 17:22:37 -0700

    updates

--- migrations.py
+++ migrations.py
@@ -1,10 +1,8 @@
 from models import db, Contact, Note
 from faker import Factory
-import secrets 
+import secrets
 
 fake = Factory.create()
-# Spanish
-#fake = Factory.create('es_ES')
 # Reload tables
 db.drop_all()
 db.create_all()
@@ -12,17 +10,18 @@ db.create_all()
 for num in range(50):
     fullname = fake.name().split()
     name = fullname[0]
-    surname = ' '.join(fullname[1:])
+    surname = " ".join(fullname[1:])
     email = fake.email()
     phone = fake.phone_number()
     # Save in database
-    mi_contacto = Contact(
+    new_contact = Contact(
         uid=secrets.token_urlsafe(8),
         name=name,
         surname=surname,
         email=email,
-        phone=phone)
-    db.session.add(mi_contacto)
+        phone=phone,
+    )
+    db.session.add(new_contact)
 
 db.session.commit()
 
@@ -30,9 +29,7 @@ contacts = Contact.query.order_by(Contact.name).all()
 for contact in contacts:
     for i in range(5):
         new_remark = Note(
-            rid=secrets.token_urlsafe(8),
-            to = contact.uid,
-            text = fake.sentence()
-            )
+            rid=secrets.token_urlsafe(8), to=contact.uid, text=fake.sentence()
+        )
         db.session.add(new_remark)
 db.session.commit()
